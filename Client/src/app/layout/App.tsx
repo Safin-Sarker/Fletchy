cf08b3a2e01e019cca8720e18d4ff0820ebc96e0
Safin-Sarker <safinsarker1122@gmail.com>
2025-08-16 20:25:34 +0200

    add theme changing

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -1,11 +1,35 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../Models/product";
 import Catalog from "../../Features/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import NavBar from "./NavBar";
+import { Css } from "@mui/icons-material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [darkmode, setdarkmode] = useState(false);
+  const palletteType = darkmode ? "dark" : "light";
+
+  const toggleDarkMode = () => {
+    setdarkmode(!darkmode);
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: palletteType,
+      background: {
+        default: palletteType == "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
 
   useEffect(() => {
     fetch("https://localhost:5001/api/products")
@@ -16,12 +40,23 @@ function App() {
   }, []);
 
   return (
-    <>
-      <NavBar />
-      <Container maxWidth="xl" sx={{ mt: 14 }}>
-        <Catalog product={products} />
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar toggleDarkMode={toggleDarkMode} darkmode={darkmode} />
+      <Box
+        sx={{
+          minHeight: "100vh",
+          background: darkmode
+            ? "radial-gradient(circle ,#1e3aBa,#111B27)"
+            : "radial-gradient(circle ,#baecf9,#f0f9ff)",
+          py: 6,
+        }}
+      >
+        <Container maxWidth="xl" sx={{ mt: 8 }}>
+          <Catalog product={products} />
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- Client/src/app/layout/NavBar.tsx
+++ Client/src/app/layout/NavBar.tsx
@@ -1,10 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 
-export default function NavBar() {
+type props = {
+  toggleDarkMode: () => void;
+  darkmode: boolean;
+};
+
+export default function NavBar({ toggleDarkMode, darkmode }: props) {
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6">Fletchy</Typography>
+        <IconButton onClick={toggleDarkMode}>
+          {darkmode ? <DarkMode /> : <LightMode sx={{ color: "yellow" }} />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
