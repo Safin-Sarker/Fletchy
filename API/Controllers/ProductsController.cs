4742d14d5bfae2f297a8b00cac4751a833b9aea6
Safin-Sarker <safinsarker1122@gmail.com>
2025-09-07 12:58:54 +0200

    Add sorting mechanism in product Controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,9 +12,12 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
         {
-            return await context.Products.ToListAsync();
+            var query = context.Products.Sort(orderBy).AsQueryable();
+
+            
+            return await query.ToListAsync();
 
         }
 
